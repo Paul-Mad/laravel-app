ec57d23e42208c5a001ce731f86decdf7b1c01cc
Paulo H Madeira <paulin.mad@hotmail.com>
2021-07-13 11:41:32 -0300

    Criando todas as rotas, controllers e views estaticas

---
+++ app/Http/Controllers/Site/CategoryController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers\Site;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('site.category.index');
+    }
+
+
+    /**
+     * Display the specified resource.
+     *
+     * @param   $slug
+     * @return \Illuminate\Http\Response
+     */
+    public function show($slug)
+    {
+        return view('site.category.show',['slug' => $slug]);
+    }
+
+    
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
